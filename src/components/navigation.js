c6c38b28ff2a617cac0eca00e08dfcce2b6a1886
Marco Ilari <marilari88@gmail.com>
2021-06-22 18:30:10 +0200

    Modificato il link Project

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,7 +5,7 @@ import { menu } from "../styles/navigation.module.css"
 export default function Navigation() {
   return (
     <nav className={menu}>
-      <Link to="/projects">Projects</Link>
+      <Link to="/">Projects</Link>
       <Link to="/notes">Notes</Link>
       <Link to="/contacts">Contacts</Link>
     </nav>
