ad891c8fd9c00b807936b1c6877539944c05d691
Marco Ilari <marilari88@gmail.com>
2021-06-22 18:25:47 +0200

    Aggiunte pagine in allestimento in corrispondenza delle sezioni appunti e contatti

---
+++ src/pages/contacts.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const Contacts = () => {
+  return (
+    <Layout>
+      <Seo title="I miei contatti" />
+      <h1>Sezione in lavorazione</h1>
+    </Layout>
+  )
+}
+
+export default Contacts
---
+++ src/pages/notes.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const Notes = () => (
+  <Layout>
+    <Seo title="I miei appunti" />
+    <h1>Sezione in lavorazione</h1>
+  </Layout>
+)
+
+export default Notes
