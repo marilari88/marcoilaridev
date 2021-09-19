c0e247b02ea13c3c24eb13ccf117cb8aed2e7c61
marilari88 <marilari88@gmail.com>
2021-09-19 12:43:46 +0200

    Contacts - created form

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -6,7 +6,52 @@ const Contacts = () => {
   return (
     <Layout>
       <Seo title="I miei contatti" />
-      <h1>Sezione in lavorazione</h1>
+      <div className="form-wrapper">
+        <form netlify>
+          <div className="riga-form">
+            <label htmlFor="nome">Il tuo nome</label>
+            <input
+              type="text"
+              name="nome"
+              id="nome"
+              placeholder="Cognome Nome"
+              maxLength="40"
+              required
+            />
+          </div>
+          <div className="riga-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Indirizzo Email"
+              maxLength="70"
+              required
+            />
+          </div>
+          <div className="riga-form">
+            <label htmlFor="telefono">Telefono</label>
+            <input
+              type="text"
+              name="telefono"
+              id="telefono"
+              placeholder="Numero di Telefono"
+              maxLength="15"
+            />
+          </div>
+          <div className="riga-form">
+            <label htmlFor="testo">Messaggio</label>
+            <textarea
+              name="testo"
+              id="testo"
+              placeholder="Messaggio"
+              required
+            />
+          </div>
+          <input type="submit" value="Invia" />
+        </form>
+      </div>
     </Layout>
   )
 }
