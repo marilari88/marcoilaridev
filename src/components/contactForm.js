fdc46d22e6d64029e0b8305f6840cd933c9997fe
Marco Ilari <marilari88@gmail.com>
2021-09-19 19:11:44 +0200

    Contacts - Styling contact form

---
+++ src/components/contactForm.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react"
+import { contactForm, formRow } from "../styles/contactForm.module.css"
+
+function ContactForm() {
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
+  return (
+    <div className="form-wrapper">
+      <form
+        className={contactForm}
+        data-netlify="true"
+        onSubmit={() => setIsSubmitted(true)}
+      >
+        <div className={formRow}>
+          <label htmlFor="nome">Il tuo nome</label>
+          <input
+            type="text"
+            name="nome"
+            id="nome"
+            placeholder="Cognome Nome"
+            maxLength="40"
+            required
+          />
+        </div>
+        <div className={formRow}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Indirizzo Email"
+            maxLength="70"
+            required
+          />
+        </div>
+        <div className={formRow}>
+          <label htmlFor="telefono">Telefono</label>
+          <input
+            type="text"
+            name="telefono"
+            id="telefono"
+            placeholder="Numero di Telefono"
+            maxLength="15"
+          />
+        </div>
+        <div className={formRow}>
+          <label htmlFor="testo">Messaggio</label>
+          <textarea name="testo" id="testo" placeholder="Messaggio" required />
+        </div>
+        <input type="submit" className="pulsante" value="Invia" />
+      </form>
+    </div>
+  )
+}
+
+export default ContactForm
